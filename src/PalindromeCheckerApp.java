0c097c95c6234772f7cfb243a6908a069635a28d
anmolbhojwani76-max <anmolbhojwani76@gmail.com>
2026-02-25 16:01:45 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,23 @@
-/**
- * =============================================================
- * MAIN CLASS - UseCase1PalindromeCheckerApp
- * =============================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- *
- * @author Developer
- * @version 1.0
- */
-
 public class PalindromeCheckerApp {
-
-    /**
-     * Application entry point.
-     * This is the first method executed by JVM.
-     */
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        // Hardcoded string literal
+        String word = "madam";
 
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using loop
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
-
 }
